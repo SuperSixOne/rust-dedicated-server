7f54a2b059a503dd995e7ab4411bb9da4af5f822
SuperSixOne <mmihalik625@gmail.com>
2022-11-28 03:04:28 -0500

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,19 +23,20 @@ else
     printf "Skipping update as flag is set\\n"
 fi
 
-cp -a "/config/saves/." "/config/backups/"
-cp -a "${GAMESAVESDIR}/server/." "/config/backups" # useless in first run, but useful in additional runs
-rm -rf "${GAMESAVESDIR}/server"
-ln -sf "/config/saves" "${GAMESAVESDIR}/server"
-ln -sf "/config/ServerSettings.${SERVERQUERYPORT}" "${GAMESAVESDIR}/ServerSettings.${SERVERQUERYPORT}"
-
-cp /home/steam/*.ini "${GAMECONFIGDIR}/Config/LinuxServer"
-
-if [ ! -f "/config/gamefiles/Engine/Binaries/Linux/UE4Server-Linux-Shipping" ]; then
-    printf "Game binary is missing.\\n"
-    exit 1
-fi
-
-cd /config/gamefiles || exit 1
-
 exec ./Engine/Binaries/Linux/UE4Server-Linux-Shipping FactoryGame -log -NoSteamClient -unattended ?listen -Port="$SERVERGAMEPORT" -BeaconPort="$SERVERBEACONPORT" -ServerQueryPort="$SERVERQUERYPORT" -multihome="$SERVERIP" "$@"
+
+exec /app/server/RustDedicated -batchmode -nographics \
+    -server.ip 0.0.0.0 \
+    -server.port="$SERVERGAMEPORT" \
+    -rcon.ip 0.0.0.0 \
+    -rcon.port 28016 \
+    -rcon.password "goodpassword" \
+    -server.maxplayers 50 \
+    -server.hostname "Server Name" \
+    -server.identity "docker" \
+    -server.level "Procedural Map" \
+    -server.seed 123456 \
+    -server.worldsize 3000 \
+    -server.saveinterval 300 \
+    -server.globalchat true \
+    -server.description "Description Here"
